072b992c126aeac749f2da2afef53cf2a350e0d7
ashwinhr186 <ashwinhr186@Ubuntu.myguest.virtualbox.org>
2023-12-02 17:46:51 -0700

    printing the key pressed2

--- numpad/numpad.py
+++ numpad/numpad.py
@@ -75,19 +75,20 @@ def readLine(line, characters):
 	GPIO.output(line, GPIO.HIGH)
 	if(GPIO.input(C1) == 1):
     	    print(characters[0])
-            
 	if(GPIO.input(C2) == 1):
         	    print(characters[1])
 	if(GPIO.input(C3) == 1):
     	    print(characters[2])
+    if(GPIO.input(C4) == 1):
+    	    print(characters[3])
 	GPIO.output(line, GPIO.LOW)
 
 try:
 	while True:
-    	    readLine(L1, ["1","2","3"])
-    	    readLine(L2, ["4","5","6"])
-    	    readLine(L3, ["7","8","9"])
-    	    readLine(L4, ["*","0","#"])
+    	    readLine(L1, ["1","2","3","A"])
+    	    readLine(L2, ["4","5","6","B"])
+    	    readLine(L3, ["7","8","9","C"])
+    	    readLine(L4, ["*","0","#","D"])
     	    time.sleep(0.1)
 except KeyboardInterrupt:
 	print("\nApplication stopped!")
