92c478204cd951625fcbf49feeb1eb0d99f9ac6a
ashwinhr186 <ashwinhr186@Ubuntu.myguest.virtualbox.org>
2023-12-09 22:53:48 -0700

    adding client.py socket application

---
+++ client/client.py
@@ -0,0 +1,18 @@
+#TCP connection based client application in Python
+
+import socket
+import sys
+import time
+
+# Create a TCP/IP socket
+# echo-client.py
+
+HOST = "169.254.59.104"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
