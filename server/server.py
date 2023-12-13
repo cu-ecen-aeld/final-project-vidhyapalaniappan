32adea0c876eeb3051f350d4fbc9fc416064abdd
vidhya <vidhya@Ubuntu.myguest.virtualbox.org>
2023-12-13 12:39:53 -0700

    Commented server.py

--- server/server.py
+++ server/server.py
@@ -1,3 +1,11 @@
+##********************************************************************************************************************************************************
+## File name        : server.py
+## ​Description      : Code to integrate LCD, numpad and fingerprint sesnor 
+## File​ ​Author​ ​Name : Vidhya. PL & Ashwin Ravindra
+## Date             : 12/13/2023
+## **********************************************************************************************************************************************************
+
+##Importing the necessary header files
 import socket
 
 HOST = "169.254.59.104"  # Standard loopback interface address (localhost)
@@ -5,24 +13,20 @@ PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print('Server started')
-    s.bind((HOST, PORT))
-    s.listen()
+    s.bind((HOST, PORT))   #binding to the host
+    s.listen()             #listening for connection requests from client
 
-    while True:
-        conn, addr = s.accept()
-        #print(f"Connected by {addr}")
-
+    while True: 
+        conn, addr = s.accept()   #accepting connection from client
         with conn:
             while True:
-                data = conn.recv(1024)
+                data = conn.recv(1024)   #recieving data from client
                 if not data:
-                    #print(f"Connection closed by {addr}")
                     break
                 print(f"Received from client {addr}: {data.decode('utf-8')}")
 
                 try:
-                    conn.sendall(data)
-                except (BrokenPipeError, ConnectionResetError):
-                    #print(f"Connection closed by {addr}")
+                    conn.sendall(data)  #sending data back
+                except (BrokenPipeError, ConnectionResetError):  
                     break
 
