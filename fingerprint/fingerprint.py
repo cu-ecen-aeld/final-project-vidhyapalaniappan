e373d6a6d16a52d5d3646393aa92225518e9f2a8
vidhya <vidhya@Ubuntu.myguest.virtualbox.org>
2023-12-13 12:52:45 -0700

    Commented and cleaned fingerprint.py

--- fingerprint/fingerprint.py
+++ fingerprint/fingerprint.py
@@ -27,152 +27,90 @@ try:
 except Exception as e:
 
     print("Our Exception message: " + str(e))
-
-    #exit(1)
+    exit(1)
 
 # function handles the process of enrolling a fingerprint
 def enrollFinger():
-
     time.sleep(2)
-
     print("Waiting for finger...")
-
     while f.readImage() == False:
-
         pass
-
     f.convertImage(0x01)
-
-    result = f.searchTemplate()
-
+    result = f.searchTemplate()    #searching if the fingerprint is already enrolled
     positionNumber = result[0]
-
     if positionNumber >= 0:
-
         print("Template already exists at position #" + str(positionNumber))
-
         time.sleep(2)
-
         return
-
     print("Remove finger...")
-
     time.sleep(2)
-
     print("Waiting for same finger again...")
-
-    while f.readImage() == False:
-
+    while f.readImage() == False:  #checking if the fingerprints match for enrolling
         pass
-
     f.convertImage(0x02)
-
     if f.compareCharacteristics() == 0:
-
         print("Fingers do not match")
-
         time.sleep(2)
-
         return
-
     f.createTemplate()
-
-    positionNumber = f.storeTemplate()
-
+    positionNumber = f.storeTemplate()  #storing the fingerprint data in the database
     print("Finger enrolled successfully!")
-
     print("New template position #" + str(positionNumber))
-
     time.sleep(2)
 
 # function handles the process of searching for a fingerprint
 def searchFinger():
-
     try:
-
         print("Waiting for finger...")
-
         while f.readImage() == False:
-
             # pass
-
             time.sleep(0.5)
-
             return
-
         f.convertImage(0x01)
-
-        result = f.searchTemplate()
-
+        result = f.searchTemplate()  #searching if the fingerprint exists
         positionNumber = result[0]
-
         accuracyScore = result[1]
-
         if positionNumber == -1:
-
             print("No match found!")
-
             time.sleep(2)
-
             return
-
         else:
-
             print("Found template at position #" + str(positionNumber))
-
             time.sleep(2)
-
     except Exception as e:
-
         print("Operation failed!")
-
         print("Exception message: " + str(e))
-
         exit(1)
-
+        
 # function handles the process of delete a fingerprint
 def deleteFinger(pos):
-
-    if f.deleteTemplate(pos) == True:
-
+    if f.deleteTemplate(pos) == True:   #delete the fingerprint data stored at position pos
         print("Template deleted!")
-
         time.sleep(2)
 
 
 while 1:
 
+#printing the menu options
     print("e) enroll print")
-
     print("s) find print")
-
     print("d) delete print")
-
     print("q) quit")
-
     print("----------------")
-
+   
     c = input("> ")
-
     if c == "e":
-
         enrollFinger()
 
     if c == "s":
-
         searchFinger()
 
-    # if c == "d":
-
-    # print("enter the position of fingerprin")
-
-    # pos = input("> ")
-
-    # deleteFinger(pos)
+    if c == "d":
+        print("enter the position of fingerprin")
+        pos = input("> ")
+        deleteFinger(pos)
 
     if c == "q":
-
         print("Exiting fingerprint example program")
-
         raise SystemExit
 
